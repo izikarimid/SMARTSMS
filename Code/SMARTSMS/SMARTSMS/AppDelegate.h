6a80c0214e2134cdad1860ce56256b1b0efe50fb
ilabadmin <ilabadmin@ilabadmins-iMac.local>
2016-07-19 18:22:37 +0300

    Updated

---
+++ Code/SMARTSMS/SMARTSMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMARTSMS
+//
+//  Created by ilabadmin on 7/19/16.
+//  Copyright (c) 2016 strathmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code/SMARTSMS/SMARTSMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMARTSMS
+//
+//  Created by ilabadmin on 7/19/16.
+//  Copyright (c) 2016 strathmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
